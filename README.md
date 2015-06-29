f1299a9bc7798381ebeb4bae83dfbd1def289014
wils0n <wiljm0.0@gmail.com>
2015-06-29 15:54:30 -0500

    add quickfind

---
+++ README.md
@@ -0,0 +1,7 @@
+##How compile and execute java with library trough terminal in linux
+
+For example QuickFindUF.java
+
+	javac -cp '.:../library/stdlib.jar' QuickFindUF.java
+
+	java -cp '.:../library/stdlib.jar' QuickFindUF < tinyUF.txt 
---
+++ UF/QuickFindUF.java
@@ -0,0 +1,85 @@
+
+
+public class QuickFindUF {
+    private int[] id;    // id[i] = component identifier of i
+    private int count;   // number of components
+
+    /**
+     * Initializes an empty union-find data structure with N isolated components 0 through N-1.
+     * @throws java.lang.IllegalArgumentException if N < 0
+     * @param N the number of objects
+     */
+    public QuickFindUF(int N) {
+        count = N;
+        id = new int[N];
+        for (int i = 0; i < N; i++)
+            id[i] = i; //inicializa a todos con su mismo id
+    }
+
+    /**
+     * Returns the number of components(between 1 and N)
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * Returns the component identifier for the component containing p
+     */
+    public int find(int p) {
+        validate(p);
+        return id[p];
+    }
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int N = id.length;
+        if (p < 0 || p >= N) {
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + N);
+        }
+    }
+
+    /**
+     * Are the two sites p and q in the same component?
+     * @return true if the two sites p and q are in
+     * the same component, and false otherwise
+     */
+    public boolean connected(int p, int q) {
+        validate(p);
+        validate(q);
+        return id[p] == id[q];
+    }
+  
+    /**
+     * Merges the component containing site p with the component
+     * containing site q.
+     * Todos los componentes de p, son cambiados por los indices de q
+     */
+    public void union(int p, int q) {
+        if (connected(p, q)) return;
+        int pid = id[p];
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pid) id[i] = id[q]; 
+        count--;
+    }
+
+    /**
+     * Reads in a sequence of pairs of integers (between 0 and N-1) from standard input, 
+     * where each integer represents some object;
+     * if the objects are in different components, merge the two components
+     * and print the pair to standard output.
+     */
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+
+}
